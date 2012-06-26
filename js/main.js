b0c9d1752621ae97685fbcddc8496c23aa7e5da0
Adrien Di Pasquale <adrien.dipasquale@gmail.com>
2012-06-26 16:21:22 +0200

    changed description to match the new datasources supported (example spreadsheet changed as well). moved timeline computing after datasource loading

--- js/main.js
+++ js/main.js
@@ -2,96 +2,103 @@ var f = Joshfire.factory;
 var params = f.config.template.options.params;
 var dataEvents = f.getDataSource("events");
 
-var googleUrl, timeline_data;
-
-if (dataEvents.config.db == "google" && 
-    dataEvents.config.col == "spreadsheets" && 
-    dataEvents.config.query.filter.docid !== undefined) 
+/* utility function to load script async */
+function loadScript(src, callback)
 {
-  /* if source is google spreadsheet, parse doc to get right url, to use TimelineJS google code
-     This part will be removed when standard data mapping will be completely updated */
-  var googleKey;
-  var docid = dataEvents.config.query.filter.docid;
-  var googleKeyAloneRegex = /^[0-9A-Za-z_]*$/;
-  if (googleKeyAloneRegex.test(docid)){
-    googleKey = docid;
-  } else {
-    var googleKeyUrlRegex = /key=([0-9A-Za-z_]*)(&|$)/;
-    var res = googleKeyUrlRegex.exec(docid);
-    if(res[1] !== undefined && res[1] !== ""){
-      googleKey = res[1];
+  var s, r;
+  r = false;
+  s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.src = src;
+  s.onload = s.onreadystatechange = function() {
+    if ( !r && (!this.readyState || this.readyState == 'complete') )
+    {
+      r = true;
+      callback();
     }
-  }
-  googleUrl = 
-    "https://docs.google.com/spreadsheet/pub?key=" + 
-    googleKey +
-    "&output=html";
+  };
+  document.body.appendChild(s);
 }
-else {
-  /* parse datasource entries into TimelineJS compatible event_list */
-  var event_list = [];
-  dataEvents.find({}, function(err, data){
-      _.map(data.entries, function (entry, idx) {
 
-        //startDate is either startDate or datePublished
-        var eventItem = {headline: entry.name};
-        if(entry.hasOwnProperty("startDate")){
-          eventItem.startDate = entry.startDate;
-        }
-        else if(entry.hasOwnProperty("datePublished")){
-          eventItem.startDate = entry.datePublished;
-        }
-        else if(entry.hasOwnProperty("uploadDate")){
-          eventItem.startDate = entry.uploadDate;
-        }
-        else if(entry.hasOwnProperty("dateModified")){
-          eventItem.startDate = entry.dateModified;
-        }
+var event_list = [], timeline_data, timeline_config;
+/* parse datasource entries into TimelineJS compatible event_list */
+dataEvents.find({}, function(err, data){
+  _.map(data.entries, function (entry, idx) {
 
-        //don't go any further if no date could be set
-        if(eventItem.startDate !== undefined){
+    //startDate is either startDate or datePublished
+    var eventItem = {headline: entry.name};
+    if(entry.hasOwnProperty("startDate")){
+      eventItem.startDate = entry.startDate;
+    }
+    else if(entry.hasOwnProperty("datePublished")){
+      eventItem.startDate = entry.datePublished;
+    }
+    else if(entry.hasOwnProperty("uploadDate")){
+      eventItem.startDate = entry.uploadDate;
+    }
+    else if(entry.hasOwnProperty("dateModified")){
+      eventItem.startDate = entry.dateModified;
+    }
 
-          //tweets and other statuses
-          if (entry["@type"] == "Article/Status" && entry.hasOwnProperty("author") &&
-              entry.author[0] !== undefined) {
-            if (entry.author[0].image !== undefined && entry.author[0].image.contentURL !== undefined) {
-              eventItem.asset = {};
-              eventItem.asset.media = entry.author[0].image.contentURL;
-              if (entry.author[0]["foaf:nick"] !== undefined) {
-                eventItem.asset.caption = "<a href='" + entry.author[0].url + "'>" +
-                  entry.author[0]["foaf:nick"] + "</a>";
-              }
-            }
+    //don't go any further if no date could be set
+    if(eventItem.startDate !== undefined){
+
+      //Article. Includes datasources coming from Google preformatted spreadsheets
+      if (entry["@type"] == "Article" && entry.hasOwnProperty("associatedMedia") ) {
+          eventItem.asset = {};
+          if(entry.associatedMedia.hasOwnProperty("url")){
+            eventItem.asset.media = entry.associatedMedia.url;
           }
-
-          //videos
-          if (entry["@type"] == "VideoObject") {
-            eventItem.asset = {};
-            eventItem.asset.media = entry.url;
-            if(entry.hasOwnProperty("author") && entry.author.name !== undefined){
-              eventItem.asset.credit = entry.author.name;
-            }
+          if(entry.associatedMedia.hasOwnProperty("name")){
+            eventItem.asset.caption = entry.associatedMedia.name;
           }
-
-          //if possible, fill fields that are still empty
-          if(entry.hasOwnProperty("endDate")){
-            eventItem.endDate = entry.endDate;
+          if(entry.associatedMedia.hasOwnProperty("author") && 
+            entry.associatedMedia.author.hasOwnProperty("name"))
+          {
+            eventItem.asset.credit = entry.associatedMedia.author.name;
           }
+      }
 
-          if (eventItem.asset === undefined && entry.hasOwnProperty("image") &&
-              entry.image.contentURL !== undefined) {
-            eventItem.asset = {media: entry.image.contentURL};
-          }      
-
-          if (eventItem.text === undefined && entry.hasOwnProperty("description")) {
-            eventItem.text = entry.description;
+      //tweets and other statuses
+      else if (entry["@type"] == "Article/Status" && entry.hasOwnProperty("author") &&
+          entry.author[0] !== undefined) {
+        if (entry.author[0].image !== undefined && entry.author[0].image.contentURL !== undefined) {
+          eventItem.asset = {};
+          eventItem.asset.media = entry.author[0].image.contentURL;
+          if (entry.author[0]["foaf:nick"] !== undefined) {
+            eventItem.asset.caption = "<a href='" + entry.author[0].url + "'>" +
+              entry.author[0]["foaf:nick"] + "</a>";
           }
-
-          event_list.push(eventItem);
         }
-      });
+      }
+
+      //videos
+      else if (entry["@type"] == "VideoObject") {
+        eventItem.asset = {};
+        eventItem.asset.media = entry.url;
+        if(entry.hasOwnProperty("author") && entry.author.name !== undefined){
+          eventItem.asset.credit = entry.author.name;
+        }
+      }
+
+      //if possible, fill fields that are still empty
+      if(entry.hasOwnProperty("endDate")){
+        eventItem.endDate = entry.endDate;
+      }
+
+      if (eventItem.asset === undefined && entry.hasOwnProperty("image") &&
+          entry.image.contentURL !== undefined) {
+        eventItem.asset = {media: entry.image.contentURL};
+      }      
+
+      if (eventItem.text === undefined && entry.hasOwnProperty("description")) {
+        eventItem.text = entry.description;
+      }
+
+      event_list.push(eventItem);
+    }
   });
-
+  
   /* include the formatted event_list into a TimelineJS structure */
   timeline_data = 
   {
@@ -103,25 +110,30 @@ else {
       "date": event_list
     }
   };
-}
 
-/* initial timeline config */
-var timeline_config = {
-  width: "100%",
-  height: "100%",
-  source: googleUrl || timeline_data,
-  //hash_bookmark: true,            //OPTIONAL
-  css:  'css/timeline.css',
-  js:   'js/timeline-min.js'
-};
+  console.log(timeline_data);
 
-/* depending on the template params, extend this config */
-if(params.start_at_end !== undefined){
-  _.extend(timeline_config, {start_at_end: true});
-}
-if(params.font !== undefined){
-  _.extend(timeline_config, {font: params.font});
-}
-if(params.lang !== undefined){
-  _.extend(timeline_config, {lang: params.lang});
-}
+  /* initial timeline config */
+  timeline_config = {
+    width: "100%",
+    height: "100%",
+    source: timeline_data,
+    //hash_bookmark: true,            //OPTIONAL
+    css:  'css/timeline.css',
+    js:   'js/timeline-min.js'
+  };
+
+  /* depending on the template params, extend this config */
+  if(params.start_at_end !== undefined){
+    _.extend(timeline_config, {start_at_end: true});
+  }
+  if(params.font !== undefined){
+    _.extend(timeline_config, {font: params.font});
+  }
+  if(params.lang !== undefined){
+    _.extend(timeline_config, {lang: params.lang});
+  }
+
+  loadScript("js/timeline-embed.js", function(){});
+
+});
